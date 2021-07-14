bb91dbe465ec4ddd9739a556503160e24330448e
Mateo García <jmgarcia@hugeinc.com>
2021-07-14 08:37:15 -0500

    feat: Add SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import './App.scss';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <main className="main">
+      <img
+        className="main__logo"
+        alt="Library logo"
+        src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_logotype_emblem.png"
+      />
+      <h1 className="main__title">React Template - Vite</h1>
+      <h2 className="main__stack-title">Tech Stack</h2>
+      <ul className="main__stack">
+        {['React', 'Sass', 'Typescript', 'Vite', 'ESLint', 'Prettier'].map(
+          (topic) => (
+            <li key={topic} className="main__stack-item">
+              <h3 className="main__stack-subtitle">{topic}</h3>
+            </li>
+          )
+        )}
+      </ul>
+    </main>
+  );
 }
 
 export { App };
