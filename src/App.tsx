d18335a35da0a49a49103ad725cf5034a7e88cbf
teo-garcia <juan.garcia@gorillalogic.com>
2022-08-22 20:09:37 -0500

    feat: Add initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from 'primitivex'
+
+function App() {
+  return (
+    <Box>
+      <Typography tag="h1">Hello World</Typography>
+    </Box>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+
+export default defineConfig({
+  plugins: [react(), eslint()],
+})
