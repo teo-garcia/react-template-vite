e04da8a977a201c34abd12d81db111ee5b4e6c82
teo-garcia <juan.garcia@gorillalogic.com>
2023-01-19 18:52:09 -0500

    feat: #5 use clsx instead of classnames and add more css utils

--- src/features/Banner/Banner.tsx
+++ src/features/Banner/Banner.tsx
@@ -1,23 +1,20 @@
 import styles from './Banner.module.css'
+import clsx from 'clsx'
 import { FaReact } from 'react-icons/fa'
 import { Box, Text } from 'primitive-jsx'
 import type { BannerProps } from '@lib/types/client'
-import classNames from 'classnames'
 
 const Banner = (props: BannerProps) => {
   const { title } = props
   return (
     <Box
-      className={classNames(
+      className={clsx(
         'h-100vh ta-center d-flex fd-column jc-center ai-center',
         styles.container
       )}
     >
-      <FaReact className={classNames('c-background', styles.icon)} />
-      <Text
-        tag="h1"
-        className={classNames('c-background fw-bold', styles.title)}
-      >
+      <FaReact className={clsx('c-background', styles.icon)} />
+      <Text tag="h1" className={clsx('c-background fw-bold', styles.title)}>
         {title}
       </Text>
     </Box>
